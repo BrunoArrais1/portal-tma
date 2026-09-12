a2d14fb37c7c1accd36dd1f6029a546ce697f09c
BrunoArrais1 <brunosarrais@gmail.com>
2026-09-12 15:22:28 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document
 .getElementById("btnGerar")
 .addEventListener("click", iniciar);
 
-function iniciar(){
+async function iniciar(){
 
     const arquivoINC =
         document.getElementById("arquivoINC").files[0];
@@ -19,9 +19,73 @@ function iniciar(){
         return;
     }
 
-    document.getElementById("status").innerHTML = `
-        ✅ Base INC: ${arquivoINC.name}<br>
-        ✅ Base TASK: ${arquivoTASK.name}<br>
-        ✅ Histórico: ${arquivoHistorico.name}
-    `;
+    try{
+
+        const dadosINC = await lerExcel(arquivoINC);
+        const dadosTASK = await lerExcel(arquivoTASK);
+        const dadosHistorico = await lerExcel(arquivoHistorico);
+
+        document.getElementById("status").innerHTML = `
+            ✅ Base INC: ${dadosINC.length} registros<br>
+            ✅ Base TASK: ${dadosTASK.length} registros<br>
+            ✅ Histórico TASK: ${dadosHistorico.length} registros
+        `;
+
+        console.log("INC", dadosINC);
+        console.log("TASK", dadosTASK);
+        console.log("HISTÓRICO", dadosHistorico);
+
+    }
+    catch(erro){
+
+        console.error(erro);
+
+        document.getElementById("status").innerHTML =
+            "❌ Erro ao ler as planilhas.";
+
+    }
+}
+
+function lerExcel(arquivo){
+
+    return new Promise((resolve, reject)=>{
+
+        const reader = new FileReader();
+
+        reader.onload = function(e){
+
+            try{
+
+                const data =
+                    new Uint8Array(e.target.result);
+
+                const workbook =
+                    XLSX.read(data, {type:'array'});
+
+                const nomeAba =
+                    workbook.SheetNames[0];
+
+                const worksheet =
+                    workbook.Sheets[nomeAba];
+
+                const json =
+                    XLSX.utils.sheet_to_json(
+                        worksheet,
+                        {defval:""}
+                    );
+
+                resolve(json);
+
+            }
+            catch(erro){
+
+                reject(erro);
+
+            }
+        };
+
+        reader.readAsArrayBuffer(arquivo);
+
+    });
+
 }
