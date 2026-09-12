9bdc903373b57064945dbda93b51f640a45a2260
BrunoArrais1 <brunosarrais@gmail.com>
2026-09-12 15:03:44 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ document
 
 async function iniciar(){
 
+    alert("1 - Entrou na função");
+
     const arquivoINC =
         document.getElementById("arquivoINC").files[0];
 
@@ -15,85 +17,11 @@ async function iniciar(){
 
     if(!arquivoINC || !arquivoTASK || !arquivoHistorico){
 
-        alert("Selecione os 3 arquivos.");
+        alert("2 - Falta arquivo");
+
         return;
     }
 
-    try{
+    alert("3 - Arquivos encontrados");
 
-        document.getElementById("status").innerHTML =
-        "⏳ Lendo arquivos...";
-
-        const dadosINC =
-            await lerExcel(arquivoINC);
-
-        const dadosTASK =
-            await lerExcel(arquivoTASK);
-
-        const dadosHistorico =
-            await lerExcel(arquivoHistorico);
-
-        document.getElementById("status").innerHTML =
-        `
-        ✅ INC: ${dadosINC.length} linhas<br>
-        ✅ TASK: ${dadosTASK.length} linhas<br>
-        ✅ HISTÓRICO: ${dadosHistorico.length} linhas
-        `;
-
-        console.log("INC", dadosINC);
-        console.log("TASK", dadosTASK);
-        console.log("HISTORICO", dadosHistorico);
-
-    }
-    catch(erro){
-
-        console.error(erro);
-
-        document.getElementById("status").innerHTML =
-        "❌ Erro ao ler os arquivos.";
-    }
-}
-
-function lerExcel(arquivo){
-
-    return new Promise((resolve, reject)=>{
-
-        const leitor = new FileReader();
-
-        leitor.onload = function(e){
-
-            try{
-
-                const dados =
-                    new Uint8Array(e.target.result);
-
-                const workbook =
-                    XLSX.read(dados, {type:"array"});
-
-                const nomeAba =
-                    workbook.SheetNames[0];
-
-                const worksheet =
-                    workbook.Sheets[nomeAba];
-
-                const json =
-                    XLSX.utils.sheet_to_json(
-                        worksheet,
-                        {defval:""}
-                    );
-
-                resolve(json);
-
-            }
-            catch(erro){
-
-                reject(erro);
-
-            }
-
-        };
-
-        leitor.readAsArrayBuffer(arquivo);
-
-    });
 }
